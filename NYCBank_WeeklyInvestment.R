69e7a64ff52f2d9a01e6ee1245c29b764d3d00f5
Sai Naing Lynn Oo <naing538@gmail.com>
2023-03-06 23:31:51 +0800

    updated codes

--- NYCBank_WeeklyInvestment.R
+++ NYCBank_WeeklyInvestment.R
@@ -6,18 +6,46 @@ library('tseries')
 library('lmtest')
 library('uroot')
 
-
+# Import dataset
 data_NYC <- read.csv("datasets/T4_Q6_NYCBank_WeeklyInvestments.csv")
-NYCBank <- ts(data_NYC$Investment, start = 1970, frequency = 12)
+
+# Convert TS (monthly data)
+NYCBank <- ts(T4_Q6_Data$Investment,start=1965,frequency=12)
+
+# ACF and PACF plot
 tsdisplay(NYCBank)
 
-ndiffs()
+# ADF test of unit root
+adf.test(NYCBank)
 
-# Building ARIMA
-arima_NYCBank_100 <- arima(NYCBank, order = c(1,0,0))
-arima_NYCBank_100_200 <- arima(NYCBank, order = c(1,0,0), seasonal = c(2,0,0))
+# C.H test for seasonal stationality 
+ch.test(NYCBank)
 
-# Sig. of coefficients test
+# Get number of differencing
+ndiffs(NYCBank) # ordinary differencing
+nsdiffs(NYCBank) # seasonal differencing
 
+# Get suggested models from auto arima
+auto.arima(NYCBank,trace=TRUE)
 
-# Adequacy 
+# Building ARIMI model
+arima_NYCBank_100 <- arima(NYCBank,order=c(1,0,0))
+
+# Building SARMIA model
+arima_NYCBank_100_200 <- arima(NYCBank,order=c(1,0,0),seasonal=c(2,0,0))
+
+# Check sig. of coefficients
+coeftest(arima_NYCBank_100)
+coeftest(arima_NYCBank_100_200)
+
+# Check residuals and Ljung-Box test
+checkresiduals(arima_NYCBank_100)
+checkresiduals(arima_NYCBank_100_200)
+
+# Forecasts using the best model
+forecast(arima_NYCBank_100_200,h=3)
+
+# Plot forecast and fitted model
+plot(forecast(arima_NYCBank_100_200,h=3),main="NYC Bank Average Weekly Investment",xlab="Year",ylab="Investment")
+lines(fitted(arima_NYCBank_100_200),col="red",lwd=2)
+legend("topleft",c("Actual","ARIMA(1,0,0),(2,0,0)[12]","Forecast"),col=c("black","red","blue"),lwd=2,cex=0.4)
